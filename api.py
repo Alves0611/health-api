2be1ad07fdf1826adf414fb028c90c72f74f0611
“Gabriel <g.alves0611@gmail.com>
2025-05-02 21:22:05 -0300

    adicionando aplicação base, manifestos k8s e aplication do argocd

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify
+# app/app.py
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/health")
-def health():
-    return jsonify({"status": "ok"})
+@app.route('/')
+def hello():
+    env = os.environ.get("APP_ENV", "undefined")
+    debug = os.environ.get("APP_DEBUG", "false")
+    password = os.environ.get("DB_PASSWORD", "not_set")
+    return f"""
+        <h1>🚀 Hello from poc-python!</h1>
+        <p>Environment: {env}</p>
+        <p>Debug: {debug}</p>
+        <p>DB Password (from secret): {password}</p>
+    """
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9090)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
