c8685612c228869142686999666d665f77afdef1
“Gabriel <g.alves0611@gmail.com>
2025-05-02 22:09:20 -0300

    tirando anotação do ingress

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-# app/app.py
 from flask import Flask
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/estudando-argocd')
+def estudando_argocd():
     env = os.environ.get("APP_ENV", "undefined")
     debug = os.environ.get("APP_DEBUG", "false")
     password = os.environ.get("DB_PASSWORD", "not_set")
-    return f"""
-        <h1>🚀 Hello from poc-python!</h1>
-        <p>Environment: {env}</p>
-        <p>Debug: {debug}</p>
-        <p>DB Password (from secret): {password}</p>
-    """
+    return f"✔️ ArgoCD ativo!\nEnvironment: {env}\nDebug: {debug}\nDB Password: {password}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
